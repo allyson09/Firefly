db6294db419b31297dd3e735b78cb3b3ecd9d29d
Allyson <allyson.frazier09@gmail.com>
2019-07-22 22:04:09 -0500

    Updated Git ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,15 +2,25 @@
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+import { Keys } from '../../apiKey';
+import { Injectable } from '@angular/core';
 
-export const environment = {
-  production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyB2d1V6fZdCyiLjBGC1ShrVXKHr1YlYE4s",
-    authDomain: "firefly-9a9f4.firebaseapp.com",
-    databaseURL: "https://firefly-9a9f4.firebaseio.com",
-    projectId: "firefly-9a9f4",
-    storageBucket: "",
-    messagingSenderId: "272265709107"
+@Injectable()
+export class UserService {
+  selectedUID: string;
+  loggedInUID: string;
+  testing: {};
+  constructor(private keys: Keys) { }
+
+  environment = {
+    production: false,
+    firebaseConfig : {
+      apiKey: this.keys.googleKey,
+      authDomain: "firefly-9a9f4.firebaseapp.com",
+      databaseURL: "https://firefly-9a9f4.firebaseio.com",
+      projectId: "firefly-9a9f4",
+      storageBucket: "",
+      messagingSenderId: "272265709107"
+    }
   }
-};
+}
